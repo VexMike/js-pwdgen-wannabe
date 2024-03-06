840d50040f77cafc0cbcaaaa8fead9cffbd47aee
Urbani Michele <michele.urbani094@gmail.com>
2024-03-07 00:34:06 +0100

    ADD: file HTML, file JS, create password with number

---
+++ js/app.js
@@ -0,0 +1,17 @@
+console.log('Ciao dal file app.js')
+
+let name = 'Yoda'; //string
+console.log(name)
+
+let surname = 'Master'; //string
+console.log(surname)
+
+let color = 'Green'; //string
+console.log(color)
+
+let age = 900; //number
+console.log(age)
+
+let password = 'name' + 'surname' + 'color' + age /*+ '900'*/; //string
+console.log(password)
+
