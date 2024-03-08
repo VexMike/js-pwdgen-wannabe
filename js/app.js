2d9e0b68e685151dfebfd441c3b17e440c58aadf
Urbani Michele <michele.urbani094@gmail.com>
2024-03-08 11:56:04 +0100

    ADD: Correzione esercizio

--- js/app.js
+++ js/app.js
@@ -1,22 +1,71 @@
 console.log('Ciao dal file app.js')
 
-let name = 'Yoda'; //string
-console.log(name)
+// Chiedere all'utente il suo nome:
+// - Dichiarare una variabile 'nome' e gli assegnamo il prompt dell'utente
+const nome = prompt('Inserisci il tuo nome') // string | null
+console.log(nome)
 
-let surname = 'Master'; //string
-console.log(surname)
+// Chiedere all'utente il suo cognome:
+// - Dichiarare una variabile 'cognome' e gli assegnamo il prompt dell'utente
+const cognome = prompt('Inserisci il tuo cognome') // string | null
+console.log(cognome)
 
-let color = 'Green'; //string
-console.log(color)
+// Chiedere all'utente il suo colore preferito:
+// - Dichiarare una variabile 'colore' e gli assegnamo il prompt dell'utente
+const colore = prompt('Inserisci il tuo colore preferito') // string | null
+console.log(colore)
 
-let age = 900; //number
-console.log(age)
+// Dichiarare una variabile numero che contiene il numero 900
+const numero = 900 // number
 
-let password = 'Yoda' + 'Master' + 'Green' + age /*+ '900'*/; //string
+// Generare numero casuale:
+// - Creo una funzione con una variabile 'random', utilizzo la funzione Math.floor con Math.random ed assegno min e max
+const numeroCasuale1 = Math.floor(Math.random() * 100) // number
+
+
+
+/*function random(min, max) {
+  const num = Math.floor(Math.random() * (max - min + 1)) + min;
+  return num;
+}
+
+random(1, 10);*/
+
+
+
+// Generare la password:
+// - Dichiarare una variabile 'password' e assegnargli la concatenazione di 'nome', 'cognome', 'colore' e un numero (es 28)
+const password = nome + cognome + colore + /*numero +*/ numeroCasuale1 // string | null
 console.log(password)
 
-let userPassword = prompt('Enter Password'); //string //null
-console.log('Password' + ' ' + userPassword + ' ' + 'correct');
 
-let randomInteger = Math.floor(Math.random() * 101); //number
-console.log(randomInteger)
+
+
+
+
+
+
+
+
+
+// let name = 'Yoda'; //string
+// console.log(name)
+
+// let surname = 'Master'; //string
+// console.log(surname)
+
+// let color = 'Green'; //string
+// console.log(color)
+
+// let age = 900; //number
+// console.log(age)
+
+// let password = 'Yoda' + 'Master' + 'Green' + age /*+ '900'*/; //string
+// console.log(password)
+
+// let userPassword = prompt('Enter Password'); //string //null
+// console.log('Password' + ' ' + userPassword + ' ' + 'correct');
+
+// let randomInteger = Math.floor(Math.random() * 101); //number
+// console.log(randomInteger)
+
