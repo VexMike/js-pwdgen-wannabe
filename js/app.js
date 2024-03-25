6c7a49aa6794e565ae6f3cd11f1bd5932a719282
Urbani Michele <michele.urbani094@gmail.com>
2024-03-25 17:35:44 +0100

    ADD: Fix esercizio

--- js/app.js
+++ js/app.js
@@ -1,31 +1,32 @@
-console.log('Ciao dal file app.js')
+console.log('Ciao dal file app.js');
 
 // Chiedere all'utente il suo nome:
 // - Dichiarare una variabile 'nome' e gli assegnamo il prompt dell'utente
-const nome = prompt('Inserisci il tuo nome') // string | null
-console.log(nome)
+const nome = prompt('Inserisci il tuo nome'); // string | null
+console.log(nome);
 
 // Chiedere all'utente il suo cognome:
 // - Dichiarare una variabile 'cognome' e gli assegnamo il prompt dell'utente
-const cognome = prompt('Inserisci il tuo cognome') // string | null
-console.log(cognome)
+const cognome = prompt('Inserisci il tuo cognome'); // string | null
+console.log(cognome);
 
 // Chiedere all'utente il suo colore preferito:
 // - Dichiarare una variabile 'colore' e gli assegnamo il prompt dell'utente
-const colore = prompt('Inserisci il tuo colore preferito') // string | null
-console.log(colore)
+const colore = prompt('Inserisci il tuo colore preferito'); // string | null
+console.log(colore);
 
 // Dichiarare una variabile numero che contiene il numero 900
-const numero = 900 // number
+const numero = 900; // number
 
 // Generare numero casuale:
 // - Creo una funzione con una variabile 'random', utilizzo la funzione Math.floor con Math.random ed assegno min e max
-const numeroCasuale1 = Math.floor(Math.random() * 100) // number
+const numeroCasuale1 = Math.floor(Math.random() * 100); // number
 
 
-
+// In questo esempio utilizziamo la funzione getRandomIntInclusive e assegnamo 2 valori fra le parentesi, 0 e 100, inizio e fine della funzione random
 /*
 const numeroCasuale2 = getRandomIntInclusive(0, 100)
+
 function getRandomIntInclusive(min, max) {
   const minCeiled = Math.ceil(min);
   const maxFloored = Math.floor(max);
@@ -37,37 +38,6 @@ function getRandomIntInclusive(min, max) {
 
 // Generare la password:
 // - Dichiarare una variabile 'password' e assegnargli la concatenazione di 'nome', 'cognome', 'colore' e un numero (es 28)
-const password = nome + cognome + colore + /*numero +*/ numeroCasuale1 // string | null
-console.log(password)
-
-
-
-
-
-
-
-
-
-
-
-// let name = 'Yoda'; //string
-// console.log(name)
-
-// let surname = 'Master'; //string
-// console.log(surname)
-
-// let color = 'Green'; //string
-// console.log(color)
-
-// let age = 900; //number
-// console.log(age)
-
-// let password = 'Yoda' + 'Master' + 'Green' + age /*+ '900'*/; //string
-// console.log(password)
-
-// let userPassword = prompt('Enter Password'); //string //null
-// console.log('Password' + ' ' + userPassword + ' ' + 'correct');
-
-// let randomInteger = Math.floor(Math.random() * 101); //number
-// console.log(randomInteger)
+const password = nome + cognome + colore + /*numero +*/ numeroCasuale1; // string | null
+console.log('Nuova Password' + password);
 
